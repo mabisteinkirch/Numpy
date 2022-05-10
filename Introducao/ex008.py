9e7155af688247d5982899edbea487d9ab93fcc9
mabisteinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2022-05-10 08:21:01 -0300

    função linspace do numpy

--- Introducao/ex008.py
+++ Introducao/ex008.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 # permite um array uniformemente espaçado e define quantos elementos vc qr em um determinado intervalo
-a = np.linspace (1, 10, 10)
+# Primeiro parametro: inicio,
+# Segundo parametro: fim incluindo o numero que termina,
+# Terceiro parametro quantos elementos vc quer entre 1 a 20
+a = np.linspace (1, 10, 20)
 
 print(a)
