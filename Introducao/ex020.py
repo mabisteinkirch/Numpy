14a6e52d05eb3c3e6c12899ed3ec8d1d3fde8935
mabisteinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2022-05-13 09:44:26 -0300

    geração alaeatória de valores e ipressão de todas as coluas para uma linha

--- Introducao/ex020.py
+++ Introducao/ex020.py
@@ -1,35 +1,6 @@
+import numpy as np
 
-#aula3
-#indexação e cortes (slice) de arrays
-e = np.arange(10,20)
+a = np.random.rand(3, 3)
 
-a [0]
-a [-11] #?
-
-h = np.random.rand(3, 3)
-
-h[0] #primeira linha e todas as colunas
-h[0][1]
-
-#mudar o valor
-h[0][1] = 2
-print(h)
-
-#posição x ate y - slice
-#nao inclui a ultima posição
-print(e[3:5])
-print(e[2:]) #vai ate o final
-
-#slice com mais de uma dimensão
-print(h[0:2,1])
-print(h[0:2]) #pega as duas primeiras linhas toda - todas as colunas
-print(h[0:2, 3]) #pega as duas primeiras linhas toda - terceira colunas
-
-#passo
-e = np.arange(10,20)
-print(e[2:9]) #começa posicao 2 ate 8 indo de 1 em um
-print(e[2:9:2]) #começa posicao 2 ate 8 pulando de 2 em 2
-print(e[8:2:-1]) #começa posicao 8 ate 3 invertido com passo 1
-
-#imagem
-from skimage
+#primeira linha e todas as colunas
+print(a[0])
